0a31bda6fbcc8b42ab68ea8273b07bbca2771239
lavanyamaresh23-cmd <lavanyamaresh23@gmail.com>
2026-04-20 21:59:49 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,24 @@
+package lab2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P1 {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
+        hashMap.put(1, "Apple");
+        hashMap.put(2, "Strawberry");
+        hashMap.put(3, "Pear");
+        hashMap.put(4, "Cucumber");
+        hashMap.put(5, "Grapes");
+
+        System.out.println("Iterating over entries:");
+
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + 
+                               " -> Value: " + entry.getValue());
+        }
+    }
+}
