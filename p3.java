2cef6ccfc1f92bdf45455ac898c4ca9886350c77
lavanyamaresh23-cmd <lavanyamaresh23@gmail.com>
2026-04-20 21:55:56 +0530

    Add files via upload

---
+++ p3.java
@@ -0,0 +1,24 @@
+package lab1;
+
+import java.util.*;
+
+public class p3 {
+    public static void main(String[] args) {
+
+        List<String> ls = new ArrayList<>();
+
+        ls.add("Orange");
+        ls.add("green");   // fixed typo
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        System.out.println("Before sorting:");
+        System.out.println(ls);
+
+        Collections.sort(ls);
+
+        System.out.println("After sorting:");
+        System.out.println(ls);
+    }
+}
